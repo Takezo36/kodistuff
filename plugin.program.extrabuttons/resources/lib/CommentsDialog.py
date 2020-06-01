0271d8d03aa017a2d10db16f6e1ea92811da101e
Benjamin Hebgen <hebgen@office.hd>
2020-06-01 22:06:44 +0200

    initial chat support

--- plugin.program.extrabuttons/resources/lib/CommentsDialog.py
+++ plugin.program.extrabuttons/resources/lib/CommentsDialog.py
@@ -6,57 +6,53 @@ class CommentsDialog(xbmcgui.WindowXMLDialog):
     if('replyFunction' in kvargs):
       self.setProperty('canReply', 'true')
       self.replyFunction = kvargs['replyFunction']
+    self.loadMoreFunction = kvargs['loadMoreFunction']
     count = 0
     self.CANCEL_ID = 5555
-    self.REPLY_ID = 6666
-    self.LOAD_MORE_ID = 7777
+    self.REPLY_ID = 7777
+    self.LOAD_MORE_ID = 6666
     self.comments = kvargs['comments']
-    print(str(self.comments))
     self.currentPath = None
     xbmcgui.WindowXMLDialog.__init__(self, *args, **kvargs)
     
   def onInit(self):
-    print("YYYYYYYYYYYYYYYYYYYYYYYYYY")
-    print(str(self.comments))
-    print("ZZZZZZZZZZZZZZZZZZZZZZZZZZZ")
     self.setContent()
-  def setContent(self, parent='loadmoreid=HOME'):
+  def setContent(self, parent='HOME'):
     comments = self.comments
     self.currentParent = parent
     listItems = []
     count = 0
-    if(parent == 'loadmoreid=HOME'):
+    if(parent != 'HOME'):
       li = xbmcgui.ListItem('..', '..')
       li.setPath(path=parent)
       li.setProperty("index", str(count))
       count += 1
       listItems.append(li)
     for comment in comments:
-      print(str(comment))
       label = comment['author'] + ' - ' + comment['date']
       label2 = comment['value']
       thumb = comment['thumb']
-      path = 'loadmoreid=' + comment['loadmoreid']
+      path = comment['loadmoreid']
       li = xbmcgui.ListItem(label, label2)
       li.setPath(path=path)
       li.setProperty("index", str(count))
       li.setArt({'thumb':thumb})
       listItems.append(li)
       count += 1
-    print("LUUUUUUUUUUUUUUK MAN")
-    print(str(listItems))
-    print("LUUUUUUUUUUUUUUK MAN")
-    self.addItems(listItems) 
+    self.getControl(50111).addItems(listItems) 
   def onClick(self, controlId):
+    print("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+    print(controlId)
+    print("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
     if controlId == self.REPLY_ID:
       self.showReplyDialog()
     elif controlId == self.CANCEL_ID:
       self.close()
     else:
-      li = self.getListItem(self.getCurrentListPosition)
+      li = self.getListItem(self.getCurrentListPosition())
       path = li.getPath()
       self.clearList()
-      self.setContent(self.loadMoreFunction(path),path)
+      self.setContent(self.loadMoreFunction(path))
   def getReplyId(self):
   #REDO
     if(self.currentPath):
--- plugin.program.extrabuttons/resources/lib/providers/YoutubeProvider.py
+++ plugin.program.extrabuttons/resources/lib/providers/YoutubeProvider.py
@@ -5,6 +5,8 @@ import xbmcaddon
 import os
 import urllib
 import simplejson as json
+import _thread
+import time
 from youtube_requests import get_videos as getVideos
 from youtube_requests import get_channels as getChannels
 from youtube_requests import v3_request as v3Request
@@ -16,7 +18,7 @@ class Provider:
   def __init__(self, match=None):
     if(match):
       videoId = match.group(1)
-      parts = ['snippet,statistics']
+      parts = ['snippet,statistics,liveStreamingDetails']
       params = {'part': ''.join(parts), 'id': videoId}
       self.videoInfo = v3Request(method='GET', path='videos', params=params)
       self.channelInfo = getChannels(self.videoInfo['items'][0]['snippet']['channelId'])
@@ -26,7 +28,10 @@ class Provider:
     result.append(self.getChannelButton())
     result.append(self.getUpVoteButton())
     result.append(self.getDownVoteButton())
-    result.append(self.getCommentsButton())
+    if(self.videoInfo['items'][0]['snippet']['liveBroadcastContent'] == 'live'):
+      result.append(self.getChatButton())
+    else:
+      result.append(self.getCommentsButton())
     #result.append(self.getRelatedVideos())
     return result
   def getChannelButton(self):
@@ -35,31 +40,59 @@ class Provider:
     return {'label': channelName, 'path': 'action=open&path=plugin://plugin.video.youtube/channel/'+self.channelInfo[0]['id']+'/', 'logo': channelLogo, 'isFolder': True, 'isPlayable': False, 'resolvedUrl':None}
   def getUpVoteButton(self): 
     logo = self.MEDIA_BASE + 'likes.png'
-    upVotes = self.videoInfo['items'][0]['statistics']['likeCount']
-    return {'label': upVotes, 'path': 'provider=plugin.video.youtube&action=upvote&video_id='+self.videoInfo['items'][0]['id'], 'logo': logo, 'isFolder': True, 'isPlayable':False, 'resolvedUrl':None}
+    try:
+      upVotes = self.videoInfo['items'][0]['statistics']['likeCount']
+    except:
+      upVotes = '0'
+    return {'label': upVotes, 'path': 'provider=plugin.video.youtube&action=upvote&rate=like&video_id='+self.videoInfo['items'][0]['id'], 'logo': logo, 'isFolder': True, 'isPlayable':False, 'resolvedUrl':None}
   def getDownVoteButton(self):
     logo = self.MEDIA_BASE + 'dislikes.png'
-    downVotes = self.videoInfo['items'][0]['statistics']['dislikeCount']
-    return {'label': downVotes, 'path': 'provider=plugin.video.youtube&action=downvote&video_id='+self.videoInfo['items'][0]['id'], 'logo': logo, 'isFolder': True, 'isPlayable':False, 'resolvedUrl':None}
-    
+    try:
+      downVotes = self.videoInfo['items'][0]['statistics']['dislikeCount']
+    except:
+      downVotes = '0'
+    return {'label': downVotes, 'path': 'provider=plugin.video.youtube&action=downvote&rate=dislike&video_id='+self.videoInfo['items'][0]['id'], 'logo': logo, 'isFolder': True, 'isPlayable':False, 'resolvedUrl':None}
   def getCommentsButton(self):
     logo = self.MEDIA_BASE + 'playlist.png'
     return {'label': 'Comments', 'path': 'provider=plugin.video.youtube&action=show_comment&video_id='+self.videoInfo['items'][0]['id'], 'logo': logo, 'isFolder': False, 'isPlayable':False, 'resolvedUrl':None}
+  def getChatButton(self):
+    logo = self.MEDIA_BASE + 'playlist.png'
+    return {'label': 'Chat', 'path': 'provider=plugin.video.youtube&action=show_chat&video_id='+self.videoInfo['items'][0]['id'], 'logo': logo, 'isFolder': False, 'isPlayable':False, 'resolvedUrl':None}
+  def showChat(self, videoId):
+    parts = ['snippet,statistics,liveStreamingDetails']
+    params = {'part': ''.join(parts), 'id': videoId}
+    self.videoInfo = v3Request(method='GET', path='videos', params=params)
+    from resources.lib.ChatDialog import ChatDialog
+    chatDialog = ChatDialog("plugin-extrabuttons-chat.xml", xbmcaddon.Addon("plugin.program.extrabuttons").getAddonInfo('path'), "default", "1080i", replyFunction=self.replyToChat, baseId=videoId)
+    _thread.start_new_thread(self.getChatMessages, (chatDialog,))
+    chatDialog.doModal()
+  def getChatMessages(self, chatDialog):
+    try:
+      chatId = self.videoInfo['items'][0]['liveStreamingDetails']['activeLiveChatId']
+    except:
+      xbmcgui.Dialog().ok("Failed to get live chat", "No live chat available")
+    
+    parts = ['snippet,authorDetails']
+    params = {'part': ''.join(parts), 'liveChatId': chatId}
+
+    messages = v3Request(method='GET', path='liveChat/messages', params=params)
+    
+    while not xbmc.Monitor().abortRequested():
+      for item in messages['items']:
+        msg = {}
+        msg['author'] = item['authorDetails']['displayName']
+        msg['date'] = item['snippet']['publishedAt']
+        msg['value'] = item['snippet']['displayMessage']
+        msg['thumb'] = item['authorDetails']['profileImageUrl']
+        chatDialog.updateContent(msg)
+      waitTime = messages['pollingIntervalMillis']
+      time.sleep(waitTime/1000)
+      params['pageToken'] = messages['nextPageToken']
+      messages = v3Request(method='GET', path='liveChat/messages', params=params)
   def showComments(self, videoId, page=None):
-    print('show comments called xxxxxxxxxxxxxxxxxxxxxx')
-    params = {'part': 'snippet',
-             'videoId': videoId,
-             'order': 'relevance',
-             'textFormat': 'plainText',
-             'maxResults': '50'}
-    if page:
-      params['pageToken'] = page
-    provider, context, client = getCoreComponents()
-    result = client.perform_v3_request(method='GET', path='commentThreads', params=params, no_login=True)
-    items = result['items']
+    items = self.getComments(videoId, True)
     comments = []
     for item in items:
-      print(str(item))
       comment = {}
       comment['author'] = item['snippet']['topLevelComment']['snippet']['authorDisplayName']
       comment['date'] = item['snippet']['topLevelComment']['snippet']['publishedAt']
@@ -68,24 +101,43 @@ class Provider:
       comment['loadmoreid'] = item['id']
       comment['replyId'] = item['id']
       comments.append(comment)
-    #xbmcgui.Window(12901).close()
     from resources.lib.CommentsDialog import CommentsDialog
-    commentsDialog = CommentsDialog("plugin-extrabuttons-comments.xml", xbmcaddon.Addon("plugin.program.extrabuttons").getAddonInfo('path'), "default", "1080i", comments=comments, loadMoreFunction=self.getComments, replyFunction=self.replyToComment)
+    commentsDialog = CommentsDialog("plugin-extrabuttons-comments.xml", xbmcaddon.Addon("plugin.program.extrabuttons").getAddonInfo('path'), "default", "1080i", comments=comments, loadMoreFunction=self.getComments, replyFunction=self.replyToComment, baseId=videoId)
     commentsDialog.doModal()
-  def getComments(self, parent):
-    return
+  def getComments(self, videoId, isHome = False):
+    if(isHome):
+      params = {'part': 'snippet',
+               'videoId': videoId,
+               'order': 'relevance',
+               'textFormat': 'plainText',
+               'maxResults': '50'}
+      path = 'commentThreads'
+    else:
+      params = {'part': 'snippet',
+                'parentId': videoId,
+                'textFormat': 'plainText',
+                'maxResults': '50'}
+      path = 'comments'
+    provider, context, client = getCoreComponents()
+    result = client.perform_v3_request(method='GET', path=path, params=params, no_login=True)
+    return result['items']
   def replyToComment(self, commentId):
     return
-  def getTextForCommentsDialog(self, result):
-    print(str(result))
-    return str(result)
+  def replyToChat(self, chatId):
+    return
   def doAction(self, action, params):
-    print('doAction called 88888888888888888')
-    print(str(action))
-    print(str(params))
     if('show_comment' == action):
       self.showComments(params['video_id'])
-    elif('upvote' == action):
-      return
-    elif('downvote' == action):
-      return
+    elif('show_chat' == action):
+      self.showChat(params['video_id'])
+    elif('upvote' == action or 'downvote' == action):
+      rate = self.rate(params['rate'], self.getRating(params['video_id']), params['video_id'])
+      xbmcgui.Dialog().ok("Voted " + rate, "Voted " + rate)
+  def getRating(self, videoId):
+    params = {'id': videoId}
+    return v3Request(method='GET', path='videos/getRating', params=params)['items'][0]['rating']
+  def rate(self, rate, currentRating, videoId):
+    if(currentRating==rate):
+      rate = 'none'
+    xbmc.executebuiltin("RunPlugin(plugin://plugin.video.youtube/video/rate/?rating="+rate+"&video_id="+videoId+")")
+    return rate
